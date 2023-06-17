17ba80f38ec0ac300fdd8b74850f8505a989fbe5
ybarra.magtoto <ybarramagtoto@gmail.com>
2023-06-16 22:50:23 -0700

    More cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
-import { api } from "~/utils/api";
 
 export default function Home() {
   
-
   return (
     <>
       <Head>
