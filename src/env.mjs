e45c156dfdec5902b961ee6219bc6992732092ca
ybarra.magtoto <ybarramagtoto@gmail.com>
2023-06-16 22:38:32 -0700

    Adding NODE_ENV env var

--- src/env.mjs
+++ src/env.mjs
@@ -8,7 +8,7 @@ export const env = createEnv({
    */
   server: {
     // DATABASE_URL: z.string().url(),
-    // NODE_ENV: z.enum(["development", "test", "production"]),
+    NODE_ENV: z.enum(["development", "test", "production"]),
     // NEXTAUTH_SECRET:
     //   process.env.NODE_ENV === "production"
     //     ? z.string().min(1)
@@ -40,7 +40,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     // DATABASE_URL: process.env.DATABASE_URL,
-    // NODE_ENV: process.env.NODE_ENV,
+    NODE_ENV: process.env.NODE_ENV,
     // NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
